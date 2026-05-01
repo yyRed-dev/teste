f965318f0e7bf2c8794f071abdfae09ad8bd6c7b
ALUNO C33 <alunoc33@unifor.local>
2026-04-30 21:22:30 -0300

    feat: idnex

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req,res){
-    res.send("<h1>Bem-vindo ao meu site!</h1>");
+    res.send("<h1>Ola!</h1>");
 });
 
 app.get("/produtos", function(req,res){
